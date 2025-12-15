2002d6b205cc02116e8affb16fd060f44deb660b
Faizan Khan <faizankhan@Faizans-MacBook-Air.local>
2025-12-15 12:06:31 -0600

    Brief description of changes

--- obj/Debug/net9.0/ServiceLocator.AssemblyInfo.cs
+++ obj/Debug/net9.0/ServiceLocator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceLocator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b542449e2ddf9f78c722ce6b5dd7aeea1bb6ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f7d91fa63b67a87c8d1690cec967829cdd881b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceLocator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceLocator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
